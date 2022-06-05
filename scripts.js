82d1ffaf9dec061a72232bd4840a59ce4c87995f
jklxco <jack.a.coleman@hotmail.com>
2022-06-05 20:44:14 +0700

    Add basic CSS styling

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 async function getData(url) {
   try {
-    const response = await fetch(url, { mode: "cors" });
+    const response = await fetch(url); //, { mode: "cors" });
     const parsedResponse = await response.json();
     return parsedResponse;
   } catch (err) {
@@ -9,19 +9,24 @@ async function getData(url) {
 }
 
 function getWeatherData(url) {
-  getData(url).then((data) => {
-    console.log(createDataObj(data));
-  });
+  getData(url)
+    .then((data) => {
+      return createDataObj(data);
+    })
+    .then((data) => {
+      showWeatherData(data);
+    });
 }
 
 function createDataObj(data) {
   const weatherData = {
+    location: data.name,
     temp: data.main.temp,
     feelsLike: data.main.feels_like,
     maxTemp: data.main.temp_max,
     minTemp: data.main.temp_min,
-    location: data.name,
   };
+
   return weatherData;
 }
 
@@ -30,7 +35,8 @@ function addButtonFunction() {
 
   form.addEventListener("submit", (e) => {
     const location = getLocation(e);
-    console.log(location);
+    const url = getUrl(location);
+    getWeatherData(url);
   });
 }
 
@@ -41,8 +47,21 @@ function getLocation(event) {
   return location;
 }
 
-let url =
-  "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=5d5641dbb60e66b72a7d761606b836f6";
+function getUrl(location) {
+  return `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=5d5641dbb60e66b72a7d761606b836f6&units=metric`;
+}
+
+function showWeatherData(data) {
+  const divLocation = document.getElementById("div-location");
+  divLocation.innerText = `${data.location}`;
+  const divTemp = document.getElementById("div-temp");
+  divTemp.innerText = `${data.temp} ºC`;
+  const divFeelsLike = document.getElementById("div-feels-like");
+  divFeelsLike.innerText = `${data.feelsLike} ºC`;
+  const divMaxTemp = document.getElementById("div-max-temp");
+  divMaxTemp.innerText = `${data.maxTemp} ºC`;
+  const divMinTemp = document.getElementById("div-min-temp");
+  divMinTemp.innerText = `${data.minTemp} ºC`;
+}
 
 addButtonFunction();
-getWeatherData(url);
